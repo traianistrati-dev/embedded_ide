fc78c7a4c786d81cb5cddd2b0c38ea47cd923a55
traianistrati-dev <traian.istrati@gmail.com>
2026-06-02 11:17:24 +0300

    test: some rust code &mut , borrow not related to this project

--- src/main.rs
+++ src/main.rs
@@ -22,3 +22,49 @@ fn main() -> eframe::Result<()> {
         Box::new(|cc| Ok(Box::new(AppIde::new(cc)))),
     )
 }
+/////////
+//NOT related to this project; Project TEST mutable references, ownership, borrowing, lifetimes, panics and smart pointers
+#[test]
+fn test_rust_code_smart_pointers() {}
+
+#[test]
+fn test_rust_code_ownership() {}
+
+#[test]
+fn test_rust_code() {
+    let imutable_string: String = "123ABC".to_string();
+    // let mutable_string: &mut String = &mut imutable_string.clone();
+    let mutable_string: &mut String = &mut imutable_string.to_owned();
+
+    println!("{:?} - &mut {:?}", imutable_string, mutable_string);
+    println!("{:?} - &mut {:?}", imutable_string, mutable_string);
+    println!("{:?} - &mut {:?}", imutable_string, mutable_string);
+
+    use egui::TextBuffer;
+    mutable_string.replace_with("456DEF");
+    println!("{:?} - &mut {:?}", imutable_string, mutable_string);
+    mutable_string.replace_with("456DEF-1");
+    println!("{:?} - &mut {:?}", imutable_string, mutable_string);
+    mutable_string.replace_with("456DEF-2");
+    println!("{:?} - &mut {:?}", imutable_string, mutable_string);
+
+    println!("{:?} - &mut {:?}", imutable_string, mutable_string);
+
+    assert_ne!(
+        // imutable_string, imutable_string,
+        mutable_string.to_string(),
+        "OK if the values are not equal"
+    );
+
+    if true {
+        //TODO:
+    } else {
+        std::panic!(
+            "imutable_string {:?} - {:?}\n mutable_string {:?} - {:?}",
+            imutable_string,
+            mutable_string,
+            imutable_string,
+            mutable_string
+        );
+    }
+}
